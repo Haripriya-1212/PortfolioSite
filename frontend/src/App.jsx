d3fe1ad4c63d41a765df96488c940e5d96a982ee
Haripriya <haripriya01212@gmail.com>
2025-07-06 22:01:12 +0530

    added about section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
+import HomePage from './HomePage'
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Navbar/>
+      <HomePage/>
     </>
   )
 }
---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+export default function HomePage() {
+  return (
+    <>
+        <Navbar/>
+        <div className='flex flex-row justify-start items-start py-20'>
+            <div className='h-80 w-70 bg-gradient-to-r from-purple-500 to-pink-500 text-white mr-10'>
+                {/* Avatar or something */}
+                {/* Hii */}
+            </div>
+            {/* <div className='border-2 border-purple-500 p-10'> */}
+            <div className='flex-1 p-10'>
+                <h1 className='font-primary text-5xl mb-5'>Hello, I'm Haripriya</h1>
+                <h2 className='font-secondary text-3xl text-gray-500 mb-10'>Data Analyst</h2>
+                <p className='font-primary'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, exercitationem cupiditate veniam, cum delectus sit sunt quibusdam laboriosam aspernatur, voluptatibus possimus? Praesentium amet ducimus recusandae totam aperiam dolorum rerum, non laboriosam autem et dolore architecto atque nostrum? Possimus, fugit provident.</p>
+            </div>
+        </div>
+    </>
+  )
+}
