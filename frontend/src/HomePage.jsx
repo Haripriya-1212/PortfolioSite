c11fb0564572fa2e0e601901e9bef3d1e2196b3c
Haripriya <haripriya01212@gmail.com>
2025-07-09 13:01:33 +0530

    added cards

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
 
 export default function HomePage() {
   return (
@@ -7,19 +8,20 @@ export default function HomePage() {
         <Navbar/>
         {/* About ------------------------------------------------------------- */}
         <div className='flex flex-row justify-start items-start py-20'>
-            <div className='h-80 w-70 bg-gradient-to-r from-purple-500 to-blue-500 text-white mr-10'>
-                {/* Avatar or something */}
-                {/* Hii */}
-            </div>
             {/* <div className='border-2 border-purple-500 p-10'> */}
-            <div className='flex-1 p-10'>
+            <div className='flex-1'>
                 <h1 className='font-primary text-5xl mb-5'>Hello, I'm Haripriya</h1>
                 <h2 className='font-secondary text-3xl text-gray-500 mb-10'>Data Analyst</h2>
                 <p className='font-primary'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, exercitationem cupiditate veniam, cum delectus sit sunt quibusdam laboriosam aspernatur, voluptatibus possimus? Praesentium amet ducimus recusandae totam aperiam dolorum rerum, non laboriosam autem et dolore architecto atque nostrum? Possimus, fugit provident.</p>
             </div>
+            {/* <div className='h-80 w-70 bg-gradient-to-r from-purple-500 to-blue-500 text-white ml-20'> */}
+            <div className='w-70 text-white ml-20'>
+                {/* Avatar or something */}
+                {/* Hii */}
+            </div>
         </div>
         {/* Skills ------------------------------------------------------------- */}
-        <div className='flex flex-row flex-wrap justify-start items-start gap-5 py-20'>
+        <div className='flex flex-row flex-wrap justify-start items-start gap-5'>
             <img src="src/assets/c.png" alt="Placeholder" className='w-20 h-20' />
             <img src="src/assets/cpp.png" alt="Placeholder" className='w-20 h-20' />
             <img src="src/assets/python.png" alt="Placeholder" className='w-20 h-20' />
@@ -33,6 +35,16 @@ export default function HomePage() {
             <img src="src/assets/mysql.png" alt="Placeholder" className='h-20' />
             <img src="src/assets/rpi.png" alt="Placeholder" className='h-20' />
             <img src="src/assets/splunk.png" alt="Placeholder" className='w-40' />
+        </div>
+        {/* Projects ------------------------------------------------------------- */}
+        <div className='flex flex-row gap-10 flex-wrap justify-start items-start py-20'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            
         </div>
     </>
   )
---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function () {
+  return (
+    // <div className='flex flex-col overflow-auto border-3 border-gray-700 p-5 rounded-lg shadow-lg h-110 w-100 hover:shadow-2xl hover:shadow-gray-500/70'>
+    <div className='flex flex-col overflow-auto border-3 border-gray-700 p-5 rounded-lg shadow-lg h-110 w-100 shadow-[0_0_20px_rgba(250,250,250,0.3)] hover:shadow-[0_0_20px_rgba(255,255,255,0.3)]'>
+        <div className='flex flex-row justify-between items-center mb-4'>
+            <div className='font-secondary font-semibold text-xl'>
+                Alzheimer's Ally
+            </div>
+            <div className='w-5 h-5'>
+                <img src="src/assets/icons/link.png" alt="link" />
+            </div>
+        </div>
+        <p className='mb-5'>A IoT project, also our major project - prototype of a device that helps Alzheimer patients more independent, uses ESP32 microcontroller, AI models for face recognition, scream detection and MERN website</p>
+        <img src="/src/assets/projects/p1.png" alt="project" className='rounded-lg'/>
+        {/* <div className='w-full h-10 bg-gray-700 text-gray-400 rounded-sm p-2 pl-3'>
+            Ask anything about this project
+        </div> */}
+    </div>
+  )
+}
